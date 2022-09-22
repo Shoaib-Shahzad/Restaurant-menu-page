d8e3995ec3be5c35161182d88c60c517864b058b
Shoaib-Shahzad <shoaibshahzad0313@gmail.com>
2022-09-22 01:03:01 -0700

    complete

---
+++ app.js
@@ -0,0 +1,8 @@
+function imageslider (anything){
+    document.querySelector('.starbucks').src = anything
+}
+
+function colorchange(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
